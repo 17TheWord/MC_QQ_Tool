1198d754b7719523e3094af87eecbcd44bde8cbc
17TheWord <17theword@gmail.com>
2024-05-21 22:38:38 +0800

    :sparkles: :boom: Add websocket server config and update client config

--- src/main/java/com/github/theword/mcqq/utils/Config.java
+++ src/main/java/com/github/theword/mcqq/utils/Config.java
@@ -1,5 +1,6 @@
 package com.github.theword.mcqq.utils;
 
+import com.github.theword.mcqq.constant.ConfigConstantMessage;
 import lombok.Getter;
 import org.apache.commons.io.FileUtils;
 import org.yaml.snakeyaml.Yaml;
@@ -34,17 +35,20 @@ public class Config {
     private boolean enableQuitMessage;
     private String serverName;
 
+    /**
+     * @param isModServer 是否为模组服
+     */
     public Config(boolean isModServer) {
         String configFolder = isModServer ? "config" : "plugins";
         Path configMapFilePath = Paths.get("./" + configFolder, "MC_QQ", "config.yml");
         if (!Files.exists(configMapFilePath)) {
-            logger.warn("[MC_QQ] 配置文件不存在，将自动生成");
+            logger.warn(ConfigConstantMessage.CONFIG_NOT_EXIST_AND_CREATE);
             try {
                 InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("config.yml");
                 assert inputStream != null;
                 FileUtils.copyInputStreamToFile(inputStream, configMapFilePath.toFile());
             } catch (IOException e) {
-                logger.warn("[MC_QQ] 生成配置文件失败");
+                logger.warn(ConfigConstantMessage.CONFIG_COPY_FIELD);
             }
         }
         try {
@@ -53,11 +57,16 @@ public class Config {
             Map<String, Object> configMap = yaml.load(reader);
             loadConfigValues(configMap);
         } catch (Exception e) {
-            logger.warn("[MC_QQ] 读取配置文件失败，将使用默认配置");
+            logger.warn(ConfigConstantMessage.CONFIG_READ_FAILED);
             setDefaultValues();
         }
     }
 
+    /**
+     * 加载 Websocket Server 配置
+     *
+     * @param websocketServerObject Websocket Server 配置
+     */
     private void loadWebsocketServer(Object websocketServerObject) {
         if (websocketServerObject instanceof Map) {
             @SuppressWarnings("unchecked")
@@ -67,28 +76,52 @@ public class Config {
             websocketServerPort = (int) websocketServer.get("port");
         } else {
             enableWebsocketServer = false;
-            logger.warn("[MC_QQ] 未读取到 websocket_server 配置，将不启用 Websocket Server");
+            logger.warn(ConfigConstantMessage.CONFIG_READ_WEBSOCKET_SERVER_FILED);
         }
     }
 
+    /**
+     * 加载 Websocket Client 配置
+     *
+     * @param websocketUrlListObject Websocket Client 配置
+     */
     private void loadWebsocketUrlList(Object websocketUrlListObject) {
         websocketUrlList = new ArrayList<>();
         if (websocketUrlListObject instanceof List) {
             @SuppressWarnings("unchecked")
             List<Object> list = (List<Object>) websocketUrlListObject;
             if (list.isEmpty()) {
-                logger.warn("[MC_QQ] 配置项 websocket_url_list 为空，将不启用 Websocket Client");
+                logger.warn(ConfigConstantMessage.CONFIG_READ_WEBSOCKET_CLIENT_URL_LIST_IS_EMPTY);
             } else {
                 for (Object websocketUrl : list) {
                     if (websocketUrl instanceof String) {
                         websocketUrlList.add((String) websocketUrl);
                     } else {
-                        logger.warn("Non-string websocketUrl found in websocket_url_list: " + websocketUrl);
+                        logger.warn(ConfigConstantMessage.CONFIG_WEBSOCKET_CLIENT_URL_IS_NOT_STRING + websocketUrl);
                     }
                 }
+                return;
             }
         } else {
-            logger.warn("[MC_QQ] websocket_url_list 配置错误，将不启用 Websocket Client");
+            logger.warn(ConfigConstantMessage.CONFIG_WEBSOCKET_CLIENT_URL_IS_NOT_LIST);
+        }
+        enableWebsocketClient = false;
+    }
+
+    /**
+     * 加载 Websocket Client 配置
+     *
+     * @param websocketClientObject Websocket Client 配置
+     */
+    private void loadWebsocketClient(Object websocketClientObject) {
+        if (websocketClientObject instanceof Map) {
+            @SuppressWarnings("unchecked")
+            Map<String, Object> websocketClient = (Map<String, Object>) websocketClientObject;
+            if ((boolean) websocketClient.get("enable")) {
+                enableWebsocketClient = true;
+                loadWebsocketUrlList(websocketClient.get("url_list"));
+                return;
+            }
         }
         enableWebsocketClient = false;
     }
@@ -98,20 +131,18 @@ public class Config {
      *
      * @param configMap 配置集合
      */
-
     private void loadConfigValues(Map<String, Object> configMap) {
         enableMcQQ = (boolean) configMap.get("enable_mc_qq");
         enableChatMessage = (boolean) configMap.get("enable_chat_message");
         enableReconnectMessage = (boolean) configMap.get("enable_reconnect_msg");
         reconnectMaxTimes = (int) configMap.get("reconnect_max_times");
         reconnectInterval = (int) configMap.get("reconnect_interval");
-        enableWebsocketClient = (boolean) configMap.get("enable_websocket_client");
         enableCommandMessage = (boolean) configMap.get("enable_command_message");
         enableDeathMessage = (boolean) configMap.get("enable_death_message");
         enableJoinMessage = (boolean) configMap.get("enable_join_message");
         enableQuitMessage = (boolean) configMap.get("enable_quit_message");
         serverName = (String) configMap.get("server_name");
-        loadWebsocketUrlList(configMap.get("websocket_url_list"));
+        loadWebsocketClient(configMap.get("websocket_client"));
         loadWebsocketServer(configMap.get("websocket_server"));
     }
 
