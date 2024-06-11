1f88092d3e9e0c6a799cb60834f79f084604dbaf
17TheWord <17theword@gmail.com>
2024-06-11 13:50:21 +0800

    :fire: Remove old unicode method

--- src/main/java/com/github/theword/mcqq/utils/Tool.java
+++ src/main/java/com/github/theword/mcqq/utils/Tool.java
@@ -23,43 +23,6 @@ public class Tool {
     public static HandleCommandReturnMessage handleCommandReturnMessage = null;
 
     /**
-     * 字符串转为 unicode 编码
-     *
-     * @param string 字符串
-     * @return unicode编码
-     */
-    public static String unicodeEncode(String string) {
-        char[] utfBytes = string.toCharArray();
-        StringBuilder unicodeBytes = new StringBuilder();
-        for (char utfByte : utfBytes) {
-            String hexB = Integer.toHexString(utfByte);
-            if (hexB.length() <= 2) {
-                hexB = "00" + hexB;
-            }
-            unicodeBytes.append("\\u").append(hexB);
-        }
-        return unicodeBytes.toString();
-    }
-
-    /**
-     * unicode编码转为字符串
-     *
-     * @param unicodeString unicode编码
-     * @return 字符串
-     */
-    public static String unicodeDecode(String unicodeString) {
-        StringBuilder string = new StringBuilder();
-        String[] hex = unicodeString.split("\\\\u");
-        for (int i = 1; i < hex.length; i++) {
-            // Convert hex to decimal
-            int data = Integer.parseInt(hex[i], 16);
-            // Convert the decimal to character
-            string.append((char) data);
-        }
-        return string.toString();
-    }
-
-    /**
      * 初始化
      *
      * @param isModServer                       是否为模组服务器
