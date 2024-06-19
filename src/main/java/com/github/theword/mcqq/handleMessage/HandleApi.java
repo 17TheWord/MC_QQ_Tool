d77199ce5fac4d04e8163825eadceebad0efd3a7
17TheWord <17theword@gmail.com>
2024-06-19 16:47:04 +0800

    :sparkles: feat(HandleProtocolMessage): 添加WebSocket参数到处理协议消息的方法中

--- src/main/java/com/github/theword/mcqq/handleMessage/HandleApi.java
+++ src/main/java/com/github/theword/mcqq/handleMessage/HandleApi.java
@@ -1,29 +1,35 @@
 package com.github.theword.mcqq.handleMessage;
 
-import com.github.theword.mcqq.returnBody.ActionbarReturnBody;
-import com.github.theword.mcqq.returnBody.MessageReturnBody;
-import com.github.theword.mcqq.returnBody.SendTitleReturnBody;
+import com.github.theword.mcqq.returnBody.returnModle.MyBaseComponent;
+import com.github.theword.mcqq.returnBody.returnModle.MyTextComponent;
+import com.github.theword.mcqq.returnBody.returnModle.SendTitle;
+import org.java_websocket.WebSocket;
+
+import java.util.List;
 
 public interface HandleApi {
 
     /**
      * 广播消息
      *
-     * @param messageReturnBody 消息体
+     * @param webSocket   WebSocket
+     * @param messageList 消息列表
      */
-    void handleBroadcastMessage(MessageReturnBody messageReturnBody);
+    void handleBroadcastMessage(WebSocket webSocket, List<MyTextComponent> messageList);
 
     /**
      * 广播 Send Title 消息
      *
-     * @param sendTitleReturnBody Send Title 消息体
+     * @param webSocket WebSocket
+     * @param sendTitle Send Title
      */
-    void handleSendTitleMessage(SendTitleReturnBody sendTitleReturnBody);
+    void handleSendTitleMessage(WebSocket webSocket, SendTitle sendTitle);
 
     /**
      * 广播 Action Bar 消息
      *
-     * @param actionbarReturnBody Action Bar 消息体
+     * @param webSocket   WebSocket
+     * @param messageList Action Bar 消息列表
      */
-    void handleActionBarMessage(ActionbarReturnBody actionbarReturnBody);
+    void handleActionBarMessage(WebSocket webSocket, List<MyBaseComponent> messageList);
 }
