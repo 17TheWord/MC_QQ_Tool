900f1e6a5e8feee94bd6eeb08217afc00368c789
17TheWord <17theword@gmail.com>
2024-08-05 14:52:22 +0800

    :sparkles: Refactor(WsServerTest): 重命名包名，修改测试方法名及导入，调整测试逻辑

--- src/test/java/com/github/theword/queqiao/websocket/WsServerTest.java
+++ src/test/java/com/github/theword/queqiao/websocket/WsServerTest.java
@@ -1,26 +1,30 @@
-package com.github.theword.mcqq.websocket;
+package com.github.theword.queqiao.websocket;
 
-import com.github.theword.mcqq.utils.Tool;
 import org.junit.jupiter.api.Test;
 import org.slf4j.LoggerFactory;
 
 import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-import static com.github.theword.mcqq.utils.Tool.config;
-import static com.github.theword.mcqq.utils.Tool.logger;
+import static com.github.theword.queqiao.utils.Tool.logger;
 
 class WsServerTest {
 
     @Test
-    void getUnicodeServerName() throws UnsupportedEncodingException {
+    void testUnicodeServerName() throws UnsupportedEncodingException {
         System.out.println(URLEncoder.encode("服务器", StandardCharsets.UTF_8.toString()));
     }
 
     @Test
-    void onOpen() {
+    void testDecodeServerName() throws UnsupportedEncodingException {
+        System.out.println(URLDecoder.decode("%E6%9C%8D%E5%8A%A1%E5%99%A8", StandardCharsets.UTF_8.toString()));
+    }
+
+    @Test
+    void testOnOpen() {
         logger = LoggerFactory.getLogger(WsServerTest.class);
         InetSocketAddress inetSocketAddress = new InetSocketAddress("127.0.0.1", 25565);
         WsServer wsServer = new WsServer(inetSocketAddress);
